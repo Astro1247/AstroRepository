32eca0af2df27caa2a8327b3fca6e857cbc37a28
AstroNB <AstroNB@ASTRONB-PC>
2017-02-20 12:34:40 +0200

    Students struct

---
+++ MyConsoleApp1/MyConsoleApp1/MyConsoleApp1.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+struct Group{
+	struct Students
+	{
+		int name;
+		int age;
+	};
+	Students students;
+};
+
+int main()
+{
+	const int studentsCount = 22;
+	struct Group ta61[studentsCount];
+	for ()
+	system("pause");
+	return 0;
+}
+
