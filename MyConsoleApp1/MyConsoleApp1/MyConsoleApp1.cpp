db68b4dfcb449970e16416f8e0d9c5ba10f9a24b
Astro <taynik777@gmail.com>
2017-02-27 00:55:51 +0200

    v1.0

--- MyConsoleApp1/MyConsoleApp1/MyConsoleApp1.cpp
+++ MyConsoleApp1/MyConsoleApp1/MyConsoleApp1.cpp
@@ -1,90 +1,170 @@
 #include "stdafx.h"
+#include "iostream"
 #include <cstdlib>
 #include <string>
 
 using namespace std;
 
-struct Date{
-	int day;
-	int month;
-	int year;
+/*
+*  Обьявить структуру, которая позволяет сохранять комплексное число
+*  Написать ф-цию позволяющую изменять комплексное число
+*  Написать ф-цию print которая выводит комплексное число в привычном для нас виде (2+5*int) (?)
+*  Написать ф-цию add принимающую 2 структуры и выдающую комплексное число которая является суммой тех двух
+*/
+
+
+struct cNum
+{
+	int a;
+	int b;
 };
 
-/*
- *  Обьявить структуру, которая позволяет сохранять комплексное число
- *  Написать ф-цию позволяющую изменять комплексное число
- *  Написать ф-цию print которая выводит комплексное число в привычном для нас виде (2+5*int) (?)
- *  Написать ф-цию add принимающую 2 структуры и выдающую комплексное число которая является суммой тех двух
- */
-
-bool isLeap(int year)
+void getInput(int &a, int &b)
 {
-	if (year % 4 == 0 && (year / 100) % 4 == 0) return true;
-	else return false;
-}
-
-int setDate(struct Date &date, int day, int month, int year)
-{
-	int maxDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	if (month == 2)
-		if (isLeap(year)) maxDays[1] = 29; else maxDays[1] = 28;
-	if (year < 0 || month < 1 || month > 12 || day < 1 || day > maxDays[month-1]) return 0;
-	else
-	{
-		date.day = day;
-		date.month = month;
-		date.year = year;
-	}
-	return 1;
-
-	/*if (year >= 0) date.year = year; 
-	else return 1;
-	if (month >= 1 || month <= 12) date.month = month;
-	else return 1;
-	if (day >= 1 || day <= 31) date.day = day;
-	else return 1;
-	return 0;*/
-}
-
-void getInput(int &day, int &month, int &year)
-{
-	printf("Enter day: ");
-	scanf("%i", &day);
-	printf("Enter month: ");
-	scanf("%i", &month);
-	printf("Enter year: ");
-	scanf("%i", &year);
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
 	getchar();
+}
+
+int setComplexNumber(struct cNum &complexNum, int a, int b)
+{
+	if (a == NULL || b == NULL) return 0;
+
+	complexNum.a = a;
+	complexNum.b = b;
+	return 1;
 }
 
 int main()
 {
-	struct Date date;
-	int day, month, year = {}, error = 0;
-	bool manualInput = true;
+	struct cNum complex1;
+	struct cNum complex2;
+	struct cNum sumcomplex;
+	bool stucturesInitialized = false;
+	int error, menuChoice;
+	int a, b;
 	do
 	{
-		if (manualInput)
-			getInput(day, month, year);
-		else
+		system("cls");
+		error = 0;
+		cout << "Menu:\n"
+			<< " 1. Init structures\n"
+			<< " 2. Change struct values\n"
+			<< " 3. Print structures\n"
+			<< " 4. Summ structues\n"
+			<< " 5. EXIT\n\n"
+			<< "Your choice: ";
+		cin >> menuChoice;
+		switch (menuChoice)
 		{
-			day = 31;
-			month = 9;
-			year = 2017;
+		case 1:
+		{
+			if (!stucturesInitialized)
+			{
+				cout << "Initializing first struct\n";
+				getInput(a, b);
+				setComplexNumber(complex1, a, b);
+				cout << "Initializing second struct\n";
+				getInput(a, b);
+				setComplexNumber(complex2, a, b);
+				stucturesInitialized = true;
+				cout << "Structures was initalzed successfully\n";
+			}
+			else
+			{
+				cout << "Structures are already initialized!\n";
+			}
+			system("pause");
+			break;
 		}
-		if (setDate(date, day, month, year) == 0)
+		case 2:
 		{
-			printf("Input date incorrect.\n\n");
+			if (!stucturesInitialized)
+			{
+				cout << "Error\n"
+					<< "Structures are not initialized\n";
+				system("pause");
+				break;
+			}
+			int a, b;
+			int structNum, error;
+			do
+			{
+				error = 0;
+				cout << "What struct do you want to change?\n"
+					<< " 1. First struct\n"
+					<< " 2. Second struct\n"
+					<< "Your choice: ";
+				cin >> structNum;
+				switch (structNum)
+				{
+				case 1:
+				{
+					cout << "Setting first struct\n";
+					getInput(a, b);
+					setComplexNumber(complex1, a, b);
+					cout << "Struct 1 successfully changed\n";
+					break;
+				}
+				case 2:
+				{
+					cout << "Setting second struct\n";
+					getInput(a, b);
+					setComplexNumber(complex2, a, b);
+					cout << "Struct 2 successfully changed\n";
+					break;
+				}
+				default:
+				{
+					cout << "Incorrect input\n";
+					error = 1;
+					break;
+				}
+				}
+			} while (error != 0);
+			system("pause");
+			break;
+		}
+		case 3:
+		{
+			if (!stucturesInitialized)
+			{
+				cout << "Error\n"
+					<< "Structures are not initialized\n";
+				system("pause");
+				break;
+			}
+			cout << "First  struct: " << complex1.a << " + " << complex1.b << "i\n";
+			cout << "Second struct: " << complex2.a << " + " << complex2.b << "i\n";
+			system("pause");
+			break;
+		}
+		case 4:
+		{
+			if (!stucturesInitialized)
+			{
+				cout << "Error\n"
+					<< "Structures are not initialized\n";
+				system("pause");
+				break;
+			}
+			sumcomplex.a = complex1.a + complex2.a;
+			sumcomplex.b = complex1.b + complex2.b;
+			cout << "Summ num: " << sumcomplex.a << " + " << sumcomplex.b << "i\n";
+			system("pause");
+			break;
+		}
+		case 5:
+			return 0;
+		default:
+		{
+			cout << "Incorrect input\n";
 			error = 1;
+			system("pause");
+			break;
 		}
-		else
-		{
-			printf("Input date correct.\n");
-			printf("Current date: %0.2d.%0.2d.%0.4d\n", date.day, date.month, date.year);
-			error = 0;
 		}
-	} while (error == 1 && manualInput);
-	system("pause");
-	return 0;
+	} while (true);
 }
-
